e0994ef00127864fd14846a7e3353df4c43aa410
Sapov <sapov@mail.ru>
2023-07-16 23:49:48 +0300

    add items

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import OrderCreateView, OrderItemCreateView, view_order, View_order_item
+from .views import OrderCreateView, OrderItemCreateView, view_order, View_order_item, all_files_in_order
 
 app_name = 'orders'
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('createitem/', OrderItemCreateView.as_view(), name="create_itmorder"),
     path('view_order/', view_order, name="view_order"),
     path('view_order_item/<pk>', View_order_item.as_view(), name="view_order"),
+    path('all_files_in_order/<int:order_id>', all_files_in_order, name="all_files_in_order"),
 
     # path('files/', FilesCreate.as_view())
 
--- orders/views.py
+++ orders/views.py
@@ -2,11 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
+from files.models import Product
 from .models import Order, OrderItem
 from django.views.generic.edit import CreateView, UpdateView
 
 
-class OrderCreateView(CreateView):
+class OrderCreateView(LoginRequiredMixin, CreateView):
     model = Order
     fields = ['id', 'total_price', 'organisation_payer', 'paid', 'status']
 
@@ -24,8 +25,9 @@ class OrderItemCreateView(CreateView):
 def view_order(request):
     '''Вывод файлов толоко авторизованного пользователя'''
     Orders = Order.objects.filter(Contractor=request.user)
+    product = Product.objects.filter(Contractor=request.user)
 
-    return render(request, "view_orders.html", {"Orders": Orders, 'title': 'Заказы'})
+    return render(request, "view_orders.html", {"Orders": Orders, 'product': product, 'title': 'Заказы'})
 
 
 # def view_order_item(request):
@@ -35,10 +37,31 @@ def view_order(request):
 #
 #     return render(request, "view_orders_item.html", {"Orders_item": Orders_item, 'title': 'Заказы'})
 class View_order_item(LoginRequiredMixin, UpdateView):
-    model = Order
+    model = OrderItem
     fields = ("__all__")
     # fields = ['quantity', 'material', 'FinishWork', 'Fields']
     template_name = 'order_update_form.html'
     login_url = 'login'
 
 
+# class View_order_item(LoginRequiredMixin, UpdateView):
+#     model = OrderItem
+#     fields = ("__all__")
+#     # fields = ['quantity', 'material', 'FinishWork', 'Fields']
+#     template_name = 'order_update_form.html'
+#     login_url = 'login'
+#
+
+
+def all_files_in_order(request, order_id):
+    Orders = Order.objects.filter(id=order_id)
+    print(Orders)
+    product = Product.objects.all()
+    print(product)
+    curent_order = Order.objects.get(pk=order_id)
+    print(curent_order)
+    context = {'Orders': Orders, 'product': product, 'curent_order': curent_order}
+
+    print(Orders[0].id, Orders)
+
+    return render(request, "all_files_in_order.html", context)
