ab0a76cb3f9c78da5f5f611b879d215fe6116e75
Sapov <sapov@mail.ru>
2023-07-29 19:46:04 +0300

    add model Role

--- users/models.py
+++ users/models.py
@@ -3,7 +3,19 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class Role(models.TextChoices):
+    '''Роли пользователей'''
+    CUSTOMER = 'CUSTOMER', 'Клиент'
+    MANAGER = 'MANAGER', 'Менеджер'
+    OPERATOR = 'OPERATOR', 'Оператор'
+    FINANCIER = 'FINANCIER', 'Бухгалтер'
+
+
 class User(AbstractUser):
+    role = models.CharField(
+        max_length=24,
+        choices=Role.choices,
+        default=Role.CUSTOMER)
     email = models.EmailField(
         _('email address'),
         unique=True,
