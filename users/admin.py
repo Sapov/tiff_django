acd1e0770548dcc0001bc4bda86131b3b169f33c
Sapov <sapov@mail.ru>
2023-07-29 19:56:54 +0300

    edit base add Role

--- users/admin.py
+++ users/admin.py
@@ -7,13 +7,15 @@ from users.forms import UserCreationForm
 User = get_user_model()
 
 
-# @admin.register(User)
-# class UserAdmin(UserAdmin):
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('username', 'email', 'password1', 'password2'),
-#         }),
-#     )
-#
-#     add_form = UserCreationForm
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+    add_form = UserCreationForm
+
+
