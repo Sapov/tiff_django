3aa2d526f6416edbffa462134710d4a4961b6e3b
Sapov <Sapov@mail.ru>
2023-08-01 15:25:21 +0300

    add Material_data

--- files/management/commands/data_test.py
+++ files/management/commands/data_test.py
@@ -35,10 +35,13 @@ StatusOrder_data = [
 ]
 
 Material_data = [
+    # Ширка TypePrint.objects.get_or_create(id=1)
     ('Баннер 440 грамм ламинированный', TypePrint.objects.get_or_create(id=1)[0], 165, 280, 72),
-    ('Баннер 510 грамм литой', TypePrint.objects.get_or_create(id=1)[0], 290, 600, 72),
+    ('Баннер 510 грамм литой', TypePrint.objects.get_or_create(id=1)[0], 290, 450, 72),
     ('Баннер 340 грамм ламинированный', TypePrint.objects.get_or_create(id=1)[0], 130, 260, 72),
-
-
+    ('Пленка самоклеющаяся', TypePrint.objects.get_or_create(id=1)[0], 190, 300, 72),
+    ('Баннерная сетка 370 грамм', TypePrint.objects.get_or_create(id=1)[0], 240, 390, 72),
+    ('Блюбек', TypePrint.objects.get_or_create(id=1)[0], 110, 220, 72),
+    ('Бумага 150 грамм СytiLight"', TypePrint.objects.get_or_create(id=1)[0], 170, 340, 72),
 
 ]
