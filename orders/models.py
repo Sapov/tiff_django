afbc7fb897de8596de454a4089b61231b458b507
Sapov <sapov@mail.ru>
2023-08-08 03:07:09 +0300

    change orderpay.html

--- orders/models.py
+++ orders/models.py
@@ -14,7 +14,7 @@ class StatusOrder(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.id}-{self.name}'
+        return f'{self.name}'
 
     class Meta:
         verbose_name_plural = 'Статусы'
