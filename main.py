f990a55485472dc97aa73f4a52b3389713b37edc
Sapov <sapov@mail.ru>
2023-10-17 11:28:42 +0300

    edit admin user

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
-def sol(num, k):
-    s = ""
-    for i in num:
-        s += str(i)
-    str_list = " ".join(str(int(s) + k)).split()
-    l = list(map(int, str_list))
-    print(l)
+# def twosum(lst: list, target: int) -> list[int]:
+#     n = len(lst)
+#
+#     for i in range(n):
+#         for j in range(i + 1, n):
+#             if lst[i] + lst[j] == target:
+#                 return [i, j]
+#
+#
+# print(twosum([2, 3, 4], 6))
+#
+
+# def twosum(lst: list, target: int) -> list[int]:
+#     hm = {}
+#     for i in range(len(lst)):
+#         hm[lst[i]] = i
+#     print(hm)
+#     for i in range(len(lst)):
+#         complement = target - lst[i]
+#         if complement in hm and complement != i:
+#             return [i, hm[complement]]
+#
+#
+# print(twosum([2, 3, 4], 6))
 
 
-print(sol(num=[1, 2, 0, 0], k=34))
+# def twosum(lst: list, target: int) -> list[int]:
+#     hm = {}
+#     for i in range(len(lst)):
+#         complement = target - lst[i]
+#         if complement in hm:
+#             return [i, hm[complement]]
+#         hm[lst[i]] = i
+#     return []
+#
+#
+# print(twosum([2, 3, 4], 6))
+
+
+def isPalindrome(x: int) -> bool:
+    temp = x
+    revers_nums = 0
+    while temp:
+        digit = temp % 10
+        print(digit)
+        revers_nums = revers_nums * 10 + digit
+
+        temp //= 10
+        print(temp)
+    return revers_nums == x
+
+
+print(isPalindrome(121))
