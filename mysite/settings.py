71baa166b245a66457a41153c1396d371dc0dd5e
Sapov <sapov@mail.ru>
2023-11-02 23:24:05 +0300

    edit settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -190,6 +190,6 @@ EMAIL_USE_SSL = False
 
 
 #---------------------CELERY-------------------------
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 # CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
