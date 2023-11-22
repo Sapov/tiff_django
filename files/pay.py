b491188793a5e2c5dd4b74c390a12f0ab82d7938
Sapov <sapov@mail.ru>
2023-11-22 17:06:32 +0300

    order pay

--- files/pay.py
+++ files/pay.py
@@ -19,12 +19,15 @@ load_dotenv(find_dotenv())
 
 
 class Robokassa:
-    '''Генерация ссылки на оплату для магазина'''
+    """Генерация ссылки на оплату для магазина"""
+
     MerchantLogin: str = os.getenv("MERCHANT_LOGIN")
     merchant_password_1: str = os.getenv("PASSWORD_ONE")
-    robokassa_payment_url: str = 'https://auth.robokassa.ru/Merchant/Index.aspx'
+    robokassa_payment_url: str = "https://auth.robokassa.ru/Merchant/Index.aspx"
 
-    def __init__(self, received_sum: decimal, description: str, order_number: int, user: str):
+    def __init__(
+        self, received_sum: decimal, description: str, order_number: int, user: str
+    ):
         self.received_sum = received_sum
         self.description = description
         self.SignatureValue = None
@@ -34,38 +37,35 @@ class Robokassa:
 
     @classmethod
     def calculate_signature(cls, *args) -> str:
-        """Create signature MD5.
-        """
-        return hashlib.md5(':'.join(str(arg) for arg in args).encode()).hexdigest()
+        """Create signature MD5."""
+        return hashlib.md5(":".join(str(arg) for arg in args).encode()).hexdigest()
 
     def resept(self) -> list[dict]:
-        ''' формируем dict по каждой позиции и кладем в list'''
+        """формируем dict по каждой позиции и кладем в list"""
         order_items = OrderItem.objects.filter(order=self.order_number)
         list_items = []
         for i, v in enumerate(order_items):
             new_dict = {
-                "name": f'{v.product.material} {v.product.length}x{v.product.width} см',
+                "name": f"{v.product.material} {v.product.length}x{v.product.width} см",
                 "quantity": v.product.quantity,
                 "sum": v.price_per_item,
                 "payment_method": "full_payment",
                 "payment_object": "commodity",
-                "tax": "none"
+                "tax": "none",
             }
             list_items.append(new_dict)
         logger.info(list_items)
         return list_items
 
     def generate_receipt(self):
-        '''
+        """
         https://docs.robokassa.ru/fiscalization/
         Формируем все позиции заказа в робочек
-        '''
+        """
         list_items = self.resept()
-        j = {"sno": os.getenv('SNO'),  # система налогообложения
-             "items": list_items
-             }
+        j = {"sno": os.getenv("SNO"), "items": list_items}  # система налогообложения
 
-        new_json = json.dumps(j, indent=2)
+        new_json = json.dumps(j)
         return new_json
 
     def generate_payment_link(self, is_test=1) -> str:
@@ -79,43 +79,43 @@ class Robokassa:
             self.received_sum,  # Cost of goods, RU
             self.order_number,  # Invoice number
             reciept,  # Receipt
-            self.merchant_password_1  # Merchant password
+            self.merchant_password_1,  # Merchant password
         )
 
         data = {
-            'MerchantLogin': self.MerchantLogin,
-            'OutSum': self.received_sum,
-            'InvId': self.order_number,
-            'Receipt': reciept,
-            'Description': self.description,  # Description of the purchase
-            'SignatureValue': signature,
-            'IsTest': is_test,
-            'Email': self.user
-
+            "MerchantLogin": self.MerchantLogin,
+            "OutSum": self.received_sum,
+            "InvId": self.order_number,
+            "Receipt": reciept,
+            "Description": self.description,  # Description of the purchase
+            "SignatureValue": signature,
+            "IsTest": is_test,
+            "Email": self.user,
         }
-        self.pay_link = f'{self.robokassa_payment_url}?{parse.urlencode(data)}'
+        self.pay_link = f"{self.robokassa_payment_url}?{parse.urlencode(data)}"
         self._add_pay_link_in_table_order()  # добавляем ссылку в базу
         return self.pay_link
 
     @classmethod
-    def check_signature_result(cls,
-                               order_number: int,  # invoice number
-                               received_sum: int,  # cost of goods, RU
-                               received_signature: hex,  # SignatureValue
-                               password: str  # Merchant password
-                               ) -> bool:
-        ''' проверка ответа от робокассы'''
+    def check_signature_result(
+        cls,
+        order_number: int,  # invoice number
+        received_sum: int,  # cost of goods, RU
+        received_signature: hex,  # SignatureValue
+        password: str,  # Merchant password
+    ) -> bool:
+        """проверка ответа от робокассы"""
         signature = cls.calculate_signature(order_number, received_sum, password)
         if signature.lower() == received_signature.lower():
-            logger.info(f'-------PAYMENT IS TRUE------------')
+            logger.info(f"-------PAYMENT IS TRUE------------")
             return True
-        logger.info(f'-------PAYMENT IS FALSE------------')
+        logger.info(f"-------PAYMENT IS FALSE------------")
         return False
 
     def _add_pay_link_in_table_order(self):
-        '''Добавим ссылку об оплате в таблицу с ордером'''
+        """Добавим ссылку об оплате в таблицу с ордером"""
         order = Order.objects.get(id=self.order_number)
-        print(f'SAVE PAYLINK: {self.pay_link}')
+        print(f"SAVE PAYLINK: {self.pay_link}")
         order.pay_link = self.pay_link
         order.save()
 
@@ -123,7 +123,6 @@ class Robokassa:
         return self.generate_payment_link()
 
 
-if __name__ == '__main__':
-    test = Robokassa(100, 'Print banner', 1, 'rpk.reds@yandex.ru')
+if __name__ == "__main__":
+    test = Robokassa(100, "Print banner", 1, "rpk.reds@yandex.ru")
     print(test.generate_payment_link())
-
