c4aef3f37621c41daa181babc909b52c1c59d685
Sapov <sapov@mail.ru>
2023-11-28 23:52:45 +0300

    edit cofig enginx

--- orders/views.py
+++ orders/views.py
@@ -269,12 +269,12 @@ def report_complite_orders(request):
 
 
 def result(request):
-    if request.POST:
-        logger.info(f"ПРИШЕЛ GET ЗАПРОС", request.POST)
-        if "OutSum" and "InvId" in request.POST:
-            received_sum = request.POST["OutSum"]
-            order_number = request.POST["InvId"]
-            received_signature = request.POST["SignatureValue"]
+    if request.GET:
+        logger.info(f"ПРИШЕЛ GET ЗАПРОС", request.GET)
+        if "OutSum" and "InvId" in request.GET:
+            received_sum = request.GET["OutSum"]
+            order_number = request.GET["InvId"]
+            received_signature = request.GET["SignatureValue"]
 
             if Robokassa.check_signature_result(
                 received_sum,
@@ -284,10 +284,10 @@ def result(request):
             ):
                 # переключаем оплату на TRUE
                 # return render(request, "orders/success_pay.html")
-                return HttpResponse(f"OK{order_number}")
+                return render(request, "orders/success_pay.html")
 
             # http://www.orders.san-cd.ru/success/?OutSum=12.00&InvId=1&SignatureValue=356f165b0869ab28c62c6c063c44bccb&IsTest=1&Culture=ru
-        return HttpResponse(f"NNoOK{order_number}")
+        return render(request, "orders/fail_pay.html")
 
 
 def success_pay(request):
