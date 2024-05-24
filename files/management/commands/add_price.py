946d05462d043d571561075d73893b3d1426d952
Sapov <Sapov@mail.ru>
2024-05-25 01:12:39 +0300

    add price.xlsx

--- files/management/commands/add_price.py
+++ files/management/commands/add_price.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
-from files.models import Material
+from files.models import Material, FinishWork, TypePrint
 from orders.models import StatusOrder
 from .data_price import Material_data
+from .from_excel import load_excel
 
 
 # ___________________________________________________
@@ -11,12 +12,26 @@ class Command(BaseCommand):
     help = "Наполнить - прайс"
 
     def handle(self, *args, **options):
-        for item in Material_data:
-            Material.objects.get_or_create(
-                name=item[0],
-                type_print=item[1],
-                price_contractor=item[2],
-                price=item[3],
-                price_customer_retail=item[4],
-                resolution_print=item[5],
-            )
+            '''заполняем ширку из первого  листа с диапазоном b2:f9 '''
+            print('*' * 30, 'Заполняю базу стоимости материалов из файла', '*' * 30)
+            for item in load_excel('shirka', 'b2:f9'):
+                print(item)
+                Material.objects.get_or_create(
+                    name=item[0],
+                    type_print=TypePrint.objects.get_or_create(id=1)[0],
+                    price_contractor=item[1],
+                    price_agent=item[2],
+                    price_customer_retail=item[3],
+                    resolution_print=item[4]
+                )
+            # заполняем Финишку в таблицу FinishWork
+            print('*' * 30, 'Заполняю Постпечатную обработку', '*' * 30)
+            for item in load_excel('finishka', 'b2:e8'):
+                print(item)
+                FinishWork.objects.get_or_create(
+                    work=item[0],
+                    price_contractor=item[1],
+                    price_agent=item[2],
+                    price_customer_retail=item[3],
+                    # is_active=item[4]
+                )
---
+++ files/management/commands/from_excel.py
@@ -0,0 +1,20 @@
+import openpyxl
+
+
+def load_excel(sheet: str, range_cell: str):
+    '''Читаем ексель файл в список списков
+    передаем имя листа и диапазон таблицы'''
+    # wb = openpyxl.load_workbook('price.xlsx')
+    wb = openpyxl.load_workbook('files/management/commands/price.xlsx')
+    # print(wb.sheetnames) # Посмотреть имена листов в Эксель файле
+    sheet = wb[sheet]  # читаю первый лист по Ширке
+    Material_data = []
+    for row in sheet[range_cell]:
+        for cell in row:
+            item = [cell.value for cell in row]
+        Material_data.append(item)
+    return Material_data
+
+
+if __name__ == '__main__':
+    load_excel('shirka', 'b2:f9')
