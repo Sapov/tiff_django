ac663fd125498bc080c4a64afb099a3f465290e3
Sapov <Sapov@mail.ru>
2024-05-25 12:04:57 +0300

    edit add price interierka

--- files/management/commands/add_price.py
+++ files/management/commands/add_price.py
@@ -13,6 +13,12 @@ class Command(BaseCommand):
     help = "Наполнить - прайс"
 
     def handle(self, *args, **options):
+        # заполняем Типы материалов в таблицу TypePrint
+        print('*' * 30, 'заполняем Типы материалов в таблицу TypePrint', '*' * 30)
+        for type_print in load_excel('type_print', 'b2:b5'):
+            print(type_print)
+            TypePrint.objects.get_or_create(type_print=type_print[0])
+
         '''заполняем ширку из первого  листа с диапазоном b2:f9 '''
         print('*' * 30, 'Заполняю базу стоимости Широкоформатная печать материалов из файла', '*' * 30)
         for item in load_excel('shirka', 'b2:f9'):
@@ -25,6 +31,20 @@ class Command(BaseCommand):
                 price_customer_retail=item[3],
                 resolution_print=item[4]
             )
+
+        print('*' * 30, 'Заполняю базу стоимости Интерьерку печать материалов из файла', '*' * 30)
+        for item in load_excel('interierka', 'b2:f17'):
+            print(item)
+            Material.objects.get_or_create(
+                name=item[0],
+                type_print=TypePrint.objects.get_or_create(id=1)[1],
+                price_contractor=item[1],
+                price=item[2],
+                price_customer_retail=item[3],
+                resolution_print=item[4]
+            )
+
+
         # заполняем Финишку в таблицу FinishWork
         print('*' * 30, 'Заполняю Постпечатную обработку', '*' * 30)
         for item in load_excel('finishka', 'b2:e8'):
@@ -36,23 +56,18 @@ class Command(BaseCommand):
                 price_customer_retail=item[3],
                 # is_active=item[4]
             )
-        # заполняем Типы материалов в таблицу TypePrint
-        print('*' * 30, 'заполняем Типы материалов в таблицу TypePrint', '*' * 30)
-        for type_print in load_excel('type_print', 'b2:b5'):
-            print(type_print)
-            TypePrint.objects.get_or_create(type_print=type_print)
 
         print('*' * 30, 'заполняем Статус Продукта ', '*' * 30)
-        for status in load_excel('status_product', 'b2:b5'):
+        for status in load_excel('status_product', 'b2:b4'):
             print(status)
-            StatusProduct.objects.get_or_create(status=status)
+            StatusProduct.objects.get_or_create(status=status[0])
 
         print('*' * 30, 'заполняем Статус Заказа ', '*' * 30)
         for status in load_excel('status_order', 'b2:b6'):
             print(status)
-            StatusOrder.objects.get_or_create(name=status)
+            StatusOrder.objects.get_or_create(name=status[0])
 
         print('*' * 30, 'заполняем Типы доставки ', '*' * 30)
-        for type_delivery in load_excel('delivery', 'b2:b6'):
+        for type_delivery in load_excel('delivery', 'b2:b2'):
             print(type_delivery)
-            Delivery.objects.get_or_create(type_delivery=type_delivery)
+            Delivery.objects.get_or_create(type_delivery=type_delivery[0])
