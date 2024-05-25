553586eb35173bbaf7c7067fcbb1ad36c0c21531
Sapov <Sapov@mail.ru>
2024-05-25 11:13:38 +0300

    add TypePrint table load

--- files/management/commands/add_price.py
+++ files/management/commands/add_price.py
@@ -10,26 +10,32 @@ class Command(BaseCommand):
     help = "Наполнить - прайс"
 
     def handle(self, *args, **options):
-            '''заполняем ширку из первого  листа с диапазоном b2:f9 '''
-            print('*' * 30, 'Заполняю базу стоимости Широкоформатная печать материалов из файла', '*' * 30)
-            for item in load_excel('shirka', 'b2:f9'):
-                print(item)
-                Material.objects.get_or_create(
-                    name=item[0],
-                    type_print=TypePrint.objects.get_or_create(id=1)[0],
-                    price_contractor=item[1],
-                    price_agent=item[2],
-                    price_customer_retail=item[3],
-                    resolution_print=item[4]
-                )
-            # заполняем Финишку в таблицу FinishWork
-            print('*' * 30, 'Заполняю Постпечатную обработку', '*' * 30)
-            for item in load_excel('finishka', 'b2:e8'):
-                print(item)
-                FinishWork.objects.get_or_create(
-                    work=item[0],
-                    price_contractor=item[1],
-                    price_agent=item[2],
-                    price_customer_retail=item[3],
-                    # is_active=item[4]
-                )
+        '''заполняем ширку из первого  листа с диапазоном b2:f9 '''
+        print('*' * 30, 'Заполняю базу стоимости Широкоформатная печать материалов из файла', '*' * 30)
+        for item in load_excel('shirka', 'b2:f9'):
+            print(item)
+            Material.objects.get_or_create(
+                name=item[0],
+                type_print=TypePrint.objects.get_or_create(id=1)[0],
+                price_contractor=item[1],
+                price_agent=item[2],
+                price_customer_retail=item[3],
+                resolution_print=item[4]
+            )
+        # заполняем Финишку в таблицу FinishWork
+        print('*' * 30, 'Заполняю Постпечатную обработку', '*' * 30)
+        for item in load_excel('finishka', 'b2:e8'):
+            print(item)
+            FinishWork.objects.get_or_create(
+                work=item[0],
+                price_contractor=item[1],
+                price_agent=item[2],
+                price_customer_retail=item[3],
+                # is_active=item[4]
+            )
+        # заполняем Типы материалов в таблицу TypePrint
+        print('*' * 30, 'заполняем Типы материалов в таблицу TypePrint', '*' * 30)
+
+        for type_print in load_excel('type_print', 'b2:b58'):
+            print(type_print)
+            TypePrint.objects.get_or_create(type_print=type_print)
