2df7969445cb9bd1e69d4a70e5440069029f57e7
Sapov <sapov@mail.ru>
2024-07-03 07:57:15 +0300

    celery

--- mysite/settings.py
+++ mysite/settings.py
@@ -187,6 +187,7 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
 EMAIL_USE_SSL = False
 
 # # ---------------------CELERY-------------------------
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 # CELERY_BROKER_URL = 'redis://localhost:6379/0'
 # CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_BROKER_URL = 'redis://redis:6379/0'
