244725047518006f16ab9d775a1e546903a4c0da
Sapov <sapov@mail.ru>
2024-07-05 14:14:03 +0300

    add test

--- files/tests.py
+++ files/tests.py
@@ -1,23 +1,24 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from files.views import index
 
 
+class HomePageTests(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
 
-# class TestFiles(TestCase):
-    # def test_index(self):
-    #     response = self.client.get('/')
-    #     self.assertEqual(response.status_code, 302)
+    def test_home_page_template(self):
+        response = self.client.get(reverse(index))
+        self.assertTemplateUsed(response, 'index.html')
 
-    # def test_all_files(self):
-    #     response = self.client.get('/allfiles/')
-    #     self.assertEqual(response.status_code, 302)
-    #
-    # def test_price(self):
-    #     response = self.client.get('/price/')
-    #     self.assertEqual(response.status_code, 302)
+    def test_home_page_url(self):
+        ''' перенаправление на страницу с авторизацией'''
+        response = self.client.get('/')
+        self.assertRedirects(response, '/users/login/?next=/')
 
-    # def test_url_by_name(self):
-    #     resp = self.client.get(reverse('myfiles'))
-    #     self.assertEqual(resp.status_code, 200)
-
-
+    def test_home_page_function(self):
+        """ проверка что мы ссылаемся на функцию index"""
+        found = resolve('/')
+        self.assertEqual(found.func, index)
