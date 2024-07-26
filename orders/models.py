c14a3011363d1bdca8c1cbeb1f08e34ca7df21cf
Sapov <sapov@mail.ru>
2024-07-26 16:28:44 +0300

    edit link -confirm complited

--- orders/models.py
+++ orders/models.py
@@ -240,7 +240,6 @@ class UtilsModel:
             # "confirm_link": f"http://{self.domain}/orders/set_status_order/{self.order_id}/{self.generate_hash()}",
             "confirm_link": self.confirm_link_to_work,
             "confirm_status_complite": self.confirm_link_to_complited,
-
             "order_id": self.order_id,
         }
         html_message = render_to_string("mail/mail_order_for_typografyl.html", data)
