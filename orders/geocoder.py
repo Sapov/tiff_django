adaf5046043806a7a60c3aa520ebcdb81959e2e7
Sapov <sapov@mail.ru>
2024-07-28 22:02:01 +0300

    add YandexDelivery

--- orders/geocoder.py
+++ orders/geocoder.py
@@ -11,19 +11,20 @@ class Geocoder:
     https://geocode-maps.yandex.ru/1.x/?apikey=YOUR_API_KEY&geocode=бул+Мухаммед+Бин+Рашид,+дом+1&format=json
 
     '''
-    url_geocode = 'https://geocode-maps.yandex.ru/1.x/?apikey='
     GEOCODER_API_YANDEX = os.getenv('GEOCODER_API_YANDEX')
     user_agent = 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405'
 
     def __init__(self):
         self.get_url = None
 
+    @classmethod
     def geopy_address_to_coordinates(self, address: str) -> tuple:
         '''Геокодер Адрес В координаты'''
         geolocotor = Yandex(api_key=self.GEOCODER_API_YANDEX, user_agent=self.user_agent)
         location = geolocotor.geocode(address)
         return location.latitude, location.longitude
 
+    @classmethod
     def geopy_coordinates_to_address(self, latitude: float, longitude: float) -> str:
         '''Геокодер Кординаты в адрес'''
         geolocator = Yandex(api_key=self.GEOCODER_API_YANDEX, user_agent=self.user_agent)
@@ -38,7 +39,7 @@ class Geocoder:
 
 if __name__ == '__main__':
     print(Geocoder().geopy_address_to_coordinates('г. Воронеж, Никитинская, 3'))
-    print(Geocoder().geopy_coordinates_to_address(51.669467, 39.202811))
+    # print(Geocoder().geopy_coordinates_to_address(51.669467, 39.202811))
 
     # ______test_____
     # assert Geocoder().geopy_address_to_coordinates('г. Воронеж, проспект патриотов, 33Б') == (51.64096, 39.126428)
