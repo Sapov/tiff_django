7862027651e41cfaee2409e339128cac6c9bd4f2
Sapov <sapov@mail.ru>
2024-08-29 00:06:39 +0300

    migrate

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,11 +16,7 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development __settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
