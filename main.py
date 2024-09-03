40ee279322bdab34f6c54a4d3ea699aa5d5c5297
Sapov <sapov@mail.ru>
2024-09-03 22:38:53 +0300

    go prod settings.py

--- main.py
+++ main.py
@@ -1,54 +1,59 @@
-from string import digits, ascii_lowercase
+# from string import digits, ascii_lowercase
+#
+#
+# class TextInput:
+#     CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+#     CHARS_CORRECT = CHARS + CHARS.upper() + digits
+#
+#     def __init__(self, name: str, size: int = 10):
+#         self.check_name(name)
+#         self.size = size
+#         self.name = name
+#
+#     @classmethod
+#     def check_name(cls, name):
+#         if type(name) != str or len(name) < 3 or len(name) > 50:
+#             ValueError("некорректное поле name")
+#         if not set(name) < set(cls.CHARS_CORRECT):
+#             ValueError("некорректное поле name")
+#
+#     def get_html(self):
+#         return f"<p class='login'>{self.name}: <input type='text' size={self.size} />"
+#
+#
+# class PasswordInput:
+#     CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+#     CHARS_CORRECT = CHARS + CHARS.upper() + digits
+#
+#     def __init__(self, name: str, size: int = 10):
+#         self.check_name(name)
+#         self.size = size
+#         self.name = name
+#
+#     def get_html(self):
+#         return f"<p class='password'>{self.name}: <input type='text' size={self.size} />"
+#
+#     @classmethod
+#     def check_name(cls, name):
+#         if type(name) != str or len(name) < 3 or len(name) > 50:
+#             ValueError("некорректное поле name")
+#         if not set(name) < set(cls.CHARS_CORRECT):
+#             ValueError("некорректное поле name")
+#
+#
+# class FormLogin:
+#     def __init__(self, lgn, psw):
+#         self.login = lgn
+#         self.password = psw
+#
+#     def render_template(self):
+#         return "\n".join(['<form action="#">', self.login.get_html(), self.password.get_html(), '</form>'])
+#
+#
+# login = FormLogin(TextInput("Логин"), PasswordInput("Пароль"))
+# html = login.render_template()
 
 
-class TextInput:
-    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
-    CHARS_CORRECT = CHARS + CHARS.upper() + digits
+class Server:
+    pass
 
-    def __init__(self, name: str, size: int = 10):
-        self.check_name(name)
-        self.size = size
-        self.name = name
-
-    @classmethod
-    def check_name(cls, name):
-        if type(name) != str or len(name) < 3 or len(name) > 50:
-            ValueError("некорректное поле name")
-        if not set(name) < set(cls.CHARS_CORRECT):
-            ValueError("некорректное поле name")
-
-    def get_html(self):
-        return f"<p class='login'>{self.name}: <input type='text' size={self.size} />"
-
-
-class PasswordInput:
-    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
-    CHARS_CORRECT = CHARS + CHARS.upper() + digits
-
-    def __init__(self, name: str, size: int = 10):
-        self.check_name(name)
-        self.size = size
-        self.name = name
-
-    def get_html(self):
-        return f"<p class='password'>{self.name}: <input type='text' size={self.size} />"
-
-    @classmethod
-    def check_name(cls, name):
-        if type(name) != str or len(name) < 3 or len(name) > 50:
-            ValueError("некорректное поле name")
-        if not set(name) < set(cls.CHARS_CORRECT):
-            ValueError("некорректное поле name")
-
-
-class FormLogin:
-    def __init__(self, lgn, psw):
-        self.login = lgn
-        self.password = psw
-
-    def render_template(self):
-        return "\n".join(['<form action="#">', self.login.get_html(), self.password.get_html(), '</form>'])
-
-
-login = FormLogin(TextInput("Логин"), PasswordInput("Пароль"))
-html = login.render_template()
