fede5b9542b4fb5ec1eb04178cd868474905480b
Sapov <sapov@mail.ru>
2024-09-03 13:24:23 +0300

    add intervals 1 hours

--- orders/alerts.py
+++ orders/alerts.py
@@ -52,8 +52,8 @@ class Alerts:
         PeriodicTask.objects.create(
             name=f'Timer count Down order №{order_id}',
             task='timer_order_complete',
-            # interval=IntervalSchedule.objects.get(every=1, period='hours'),
-            interval=IntervalSchedule.objects.get(every=2, period='minutes'),
+            interval=IntervalSchedule.objects.get(every=1, period='hours'),
+            # interval=IntervalSchedule.objects.get(every=2, period='minutes'),
             args=json.dumps([order_id, domain]),
             # start_time=Orders.date_complete - datetime.timedelta(hours=3),  # за три часа до дедлайна пишем письма
             start_time=timezone.now() + datetime.timedelta(minutes=3),  # за три часа до дедлайна пишем письма
