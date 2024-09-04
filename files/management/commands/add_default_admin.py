767c3776a0f41da23a79903057e81473b01183fb
Sapov <sapov@mail.ru>
2024-09-04 17:23:36 +0300

    edit add_admin

--- files/management/commands/add_default_admin.py
+++ files/management/commands/add_default_admin.py
@@ -1,13 +1,15 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+import os
 User = get_user_model()
 
 
 class Command(BaseCommand):
-    USER_NAME = 'admin'
-    USER_MAIL = 'admin@admin.ru'
-    USER_PASSWORD = 'admin'
+    USER_NAME = os.getenv('USER_NAME')
+    USER_MAIL = os.getenv('USER_MAIL')
+    USER_PASSWORD = os.getenv('USER_PASSWORD')
 
     def handle(self, *args, **options):
         print('[INFO] Добавляю супер пользователя:', self.USER_NAME, '\n',
