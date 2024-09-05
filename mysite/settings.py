7ea886acbc553dc59143ce4b7479d17634804ddc
Sapov <sapov@mail.ru>
2024-09-05 16:56:47 +0300

    KAN-39 edit confirm_order_to_work.html

--- mysite/settings.py
+++ mysite/settings.py
@@ -181,10 +181,10 @@ EMAIL_USE_SSL = False
 
 # # ---------------------CELERY-------------------------
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
-# CELERY_BROKER_URL = 'redis://localhost:6379/0'
-# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+# CELERY_BROKER_URL = 'redis://redis:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
 CELERY_IMPORTS = [
     'orders.tasks',
