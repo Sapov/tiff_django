700503b2746ff64de808f0788567125754fa0d3d
Sapov <sapov@mail.ru>
2024-09-15 13:57:59 +0300

    add link dashbord to base.html

--- orders/urls.py
+++ orders/urls.py
@@ -15,7 +15,7 @@ from .views import (
     report_complite_orders,
     new_order,
     OrdersViewList,
-    AllOrdersListView, result, success_pay, fail, report_day, set_status_order, ViewAllCompleteOrders
+    AllOrdersListView, result, success_pay, fail, report_day, set_status_order
 )
 
 app_name = "orders"
