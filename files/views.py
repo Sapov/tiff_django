4d2606a2754b102d370fb4c49328a044d639065a
Sapov <sapov@mail.ru>
2024-12-01 22:38:39 +0300

    editing...

--- files/views.py
+++ files/views.py
@@ -454,13 +454,12 @@ def add_time_order(request, pk: int, hash_code):
 
 
 def about_file(request, file_id):
-    print(file_id)
     file = Product.objects.get(id=file_id)
     print(f'Разрешение файла {file.resolution} VS Разрешение печати {file.material.resolution_print}')
     if file.resolution < file.material.resolution_print:
-        message = f"Файл не подходит для качественной печати. Разрешение файла {file.resolution} dpi меньше положенного {file.material.resolution_print} dpi"
+        message = (f"Файл не подходит для качественной печати. Разрешение файла {file.resolution} dpi меньше "
+                   f"положенного {file.material.resolution_print} dpi")
     else:
         message = ''
-    print(file)
 
     return render(request, "files/about_file.html", {"file": file, 'message': message})
