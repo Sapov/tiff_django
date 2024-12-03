52333eb74cde2c70f0196c80b5b04e8b430ae1f4
Sapov <sapov@mail.ru>
2024-12-03 11:12:03 +0300

    edit about_file.html

--- main.py
+++ main.py
@@ -29,95 +29,22 @@
 # т.к. width - 4 /
 #
 # '''
-
-import asyncio
-
-import PIL
-from PIL import Image, ImageOps
-from math import ceil
-import aiohttp
-from aiohttp import ClientSession
-
-# async def fetch_status(session: ClientSession, url: str) -> int:
-#     async with session.get(url) as result:
-#         return result.status
-#
-# async def main():
-#     async with aiohttp.ClientSession() as session:
-#         url = 'https://san-cd.ru'
-#         status = await fetch_status(session, url)
-#         print(f'Состояние для {url} было равно {status}')
-#
-img = Image.open('/home/sasha/Загрузки/test_pic/res_50 Dpi.tif')
+def find_primes():
+    primes = []
+    for possible_prime in range(2, 20):
+        is_prime = True
+        for num in range(2, int(possible_prime ** 0.5) + 1):
+            if possible_prime % num == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(possible_prime)
+    yield primes
 
 
-class ImageFile:
-    '''Работа с загруженным файлом
-    dimensions - параметры файла
-    resolution_reduction - изменение разрешения файла
-    '''
+# Example usage:
+# print(find_primes(30))  # Output: [2, 3, 5, 7, 11, 13, 17, 19,
 
-    def __init__(self, image):
-        self.width_px = None
-        self.length_px = None
-        self.image = image
-        self.length = None
-        self.width = None
-        self.resolution = None
-
-    def dimensions(self) -> tuple:
-        """
-        @return:
-        width: float
-        length: float
-        resolution: float
-        """
-        try:
-            Image.MAX_IMAGE_PIXELS = None
-            with Image.open(self.image) as img:
-                self.width_px, self.length_px = img.size
-                print(f'width: {self.width_px} px, length: {self.length_px} px')
-                self.resolution = int(round(img.info['dpi'][0], 0))
-                print('RESOLUTION', img.info['dpi'])
-                self.width = round(2.54 * self.width_px / self.resolution, 0) / 100
-                self.length = round(2.54 * self.length_px / self.resolution, 0) / 100
-        except PIL.UnidentifiedImageError:
-
-            return '''!!! -- Это ошибка: Не сведенный файл Tiff --- !!!
-    Решение: Photoshop / слои / выполнить сведение'''
-
-        return self.width, self.length, self.resolution
-
-    def resolution_reduction(self, new_resolution: int):
-        self.dimensions()
-        multiplier_width = self.width_px / self.resolution
-        multiplier_length = self.length_px / self.resolution
-        new_width_px = ceil(multiplier_width * new_resolution)
-        new_length_px = ceil(multiplier_length * new_resolution)
-        self.width = ceil(2.54 * multiplier_width * new_resolution / new_resolution) / 100
-        self.length = ceil(2.54 * multiplier_length) / 100
-        print(f'[RESIZE File] New Size:{self.width} m X {self.length} m')
-        try:
-            Image.MAX_IMAGE_PIXELS = None
-            with Image.open(self.image) as file:
-                file = file.resize(size=(new_width_px, new_length_px))
-                file.save(self.image, compression='tiff_lzw',
-                          dpi=(new_resolution, new_resolution))
-
-        except Exception as Ex:
-            print(Ex)
-
-    def draw_outline_image(self):
-        # Делаем обводку вокруг файла, часто файлы имею много белого  - непонятно как его разрезать
-        res = self.dimensions()[2]
-        file = Image.open(self.image)
-        img_border = ImageOps.expand(file, border=2, fill='black')
-        img_border.save(self.image, compression='tiff_lzw', dpi=(res, res))
-
-
-# im = ImageFile('/home/sasha/Загрузки/test_pic/85x200.tif')
-# im.resolution_reduction(40)
-im_new = ImageFile('/home/sasha/Загрузки/test_pic/85x200.tif')
-
-ImageFile('/home/sasha/Загрузки/test_pic/85x200.tif').draw_outline_image()
-im_new.dimensions()
+a = find_primes()
+for i in range(20):
+    print(next(a))
