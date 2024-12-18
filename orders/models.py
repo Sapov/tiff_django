e44e3d0c00a090cddb71345027f806ecee73f8a0
Sapov <sapov@mail.ru>
2024-12-18 15:17:51 +0300

    duble quanity deletting

--- orders/models.py
+++ orders/models.py
@@ -123,12 +123,12 @@ class OrderItem(models.Model):
     def save(self, *args, **kwargs):
         self.quantity = self.product.quantity
         self.price_per_item = self.product.price
-        self.total_price = self.price_per_item * self.quantity
+        self.total_price = self.price_per_item  # Убрал * self.quantity
         # Cost
         cost_price_per_item = self.product.cost_price
         logger.info(f"cost_price_per_item {cost_price_per_item}")
         self.cost_price_per_item = cost_price_per_item
-        self.cost_total_price = self.cost_price_per_item * self.quantity
+        self.cost_total_price = self.cost_price_per_item # Убр ал  * self.quantity
 
         super(OrderItem, self).save(*args, **kwargs)
 
