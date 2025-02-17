f17aca73894e1f31d30f9598e5c4e83572123a65
Sapov <sapov@mail.ru>
2025-02-17 18:15:39 +0300

    edit bf form model

--- bforms/views.py
+++ bforms/views.py
@@ -2,10 +2,10 @@ import os
 
 from django.shortcuts import render
 from bforms.forms import SaleBanner
+from users.tasks import send_message_whatsapp
 
 from django.views.generic.edit import CreateView
 from .models import SiteOrder
-from django.urls import reverse
 
 
 class SiteOrderCreateView(CreateView):
@@ -34,10 +34,10 @@ def sale_banner(request):
 
 
 def order_complete(request):
-    # ----------''' Сообщение дминистратору'''--------------
+    # ----------''' Сообщение Администратору'''--------------
     ''' успешная отправка письма В будущем - -Сообщение менеджеру типографии'''
     admin_phone = os.getenv('PHONE_NUMBER')
-    from users.tasks import send_message_whatsapp
     send_message_whatsapp.delay(f'{admin_phone}', f'Новая заявка на сайте !')
-
+    resp = request.POST
+    print(resp)
     return render(request, 'bforms/order_complete.html')
