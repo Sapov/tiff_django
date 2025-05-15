e30eada0f3af79d60f2662d30b7f7232d8ae532c
Sapov <sapov@mail.ru>
2025-05-15 21:19:52 +0300

    add tasks.py in Files

--- files/tasks.py
+++ files/tasks.py
@@ -1,7 +1,7 @@
 from celery import shared_task
-
 from files.works_with_files.image_tiff_file import ImageFile
-
+from celery_progress.backend import ProgressRecorder
+import time
 
 @shared_task
 def resize_image(file):
@@ -9,5 +9,13 @@ def resize_image(file):
     item_file.resolution_reduction(file.material.resolution_print)
 
 
-if __name__ == "__main__":
-    main()
+@shared_task(bind=True)
+def process_uploaded_file(self, file_path, user_id):
+    progress_recorder = ProgressRecorder(self)
+
+    # Имитация обработки файла
+    for i in range(100):
+        time.sleep(0.1)  # Замените на реальную обработку
+        progress_recorder.set_progress(i + 1, 100, description='Обработка файла')
+
+    return {'result': 'success'}
