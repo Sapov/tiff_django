f838ac2e9260957ec6c94c43885fc227dbf33be3
Sapov <sapov@mail.ru>
2025-05-16 01:39:40 +0300

    edit settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap4",
     'celery_progress',
     'info',
+    'kb.apps.KbConfig',
     'rest_framework',
     "phonenumber_field",
     'django_extensions',
