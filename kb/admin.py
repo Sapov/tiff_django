1aaad7f17930c5c7b72dc86922479c014242583f
Sapov <sapov@mail.ru>
2025-05-16 01:34:43 +0300

    add admin.py

--- kb/admin.py
+++ kb/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Article, Comment
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author', 'created_at', 'is_published')
+    list_filter = ('category', 'is_published', 'created_at')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('article', 'author', 'created_at')
+    list_filter = ('created_at', 'author')
+    search_fields = ('text', 'article__title')
