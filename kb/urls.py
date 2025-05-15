e0709dd96176f6ea1300ea537867b279e31d4da6
Sapov <sapov@mail.ru>
2025-05-16 01:36:21 +0300

    add urls

---
+++ kb/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+    add_comment,
+)
+
+urlpatterns = [
+    path('', ArticleListView.as_view(), name='article_list'),
+    path('category/<slug:category_slug>/', ArticleListView.as_view(), name='article_list_by_category'),
+    path('article/<slug:slug>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('article/new/', ArticleCreateView.as_view(), name='article_create'),
+    path('article/<slug:slug>/edit/', ArticleUpdateView.as_view(), name='article_update'),
+    path('article/<slug:slug>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('article/<slug:slug>/comment/', add_comment, name='add_comment'),
+]
