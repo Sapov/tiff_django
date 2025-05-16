374644a266f379f13833e5021e8a1ef2d6972cac
Sapov <sapov@mail.ru>
2025-05-16 15:30:46 +0300

    edit form ArticleForm

--- kb/forms.py
+++ kb/forms.py
@@ -4,7 +4,10 @@ from .models import Article, Comment
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'content', 'category', 'is_published']
+        fields = ['title', 'content', 'category', 'is_published', 'image']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 10}),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
