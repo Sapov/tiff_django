c687a77eb85d1a43457e0610f7e698d001548ecd
Sapov <sapov@mail.ru>
2025-05-26 17:38:07 +0300

    create design_form.html for design

--- design/urls.py
+++ design/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import DesignDetailView
 
 urlpatterns = [
-    path('', DesignDetailView.as_view(), name='detail_order'),
+    path('/<pk:int>', DesignDetailView.as_view(), name='detail_order'),
     # path('category/<slug:category_slug>/', ArticleListView.as_view(), name='article_list_by_category'),
     # path('article/new/', ArticleCreateView.as_view(), name='article_create'),
     # path('article/<slug:slug>/', ArticleDetailView.as_view(), name='article_detail'),
--- design/views.py
+++ design/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import *
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+
+
+class CreteDesignOrder(CreateView):
+    model = DesignOrder
+
 
 class DesignDetailView(DetailView):
     model = DesignOrder
     template_name = 'design/design_detail.html'
+
+
+
 
