bfaf0e9860b9b1ac9210adf46077288a35d5cd57
Sapov <sapov@mail.ru>
2025-05-27 17:04:54 +0300

    create model Project

--- designs/models.py
+++ designs/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    client = models.ForeignKey(User, on_delete=models.CASCADE, related_name='client_projects')
+    designer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='designer_projects')
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
