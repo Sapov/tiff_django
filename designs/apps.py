54c9f55ab0cf2c50962f5515d7a3e51c65dab19f
Sapov <sapov@mail.ru>
2025-06-05 21:04:20 +0300

    app

--- designs/apps.py
+++ designs/apps.py
@@ -5,9 +5,6 @@ class DesignsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'designs'
 
-
-
-
-
     def ready(self):
-        import designs.signals  # Регистрируем сигналы
+        # Импортируем сигналы только после полной загрузки приложения
+        from . import signals
