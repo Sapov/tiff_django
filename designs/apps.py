30a68578fb8fa1e91afbe857ed3f9d09f5043e6c
Sapov <sapov@mail.ru>
2025-06-05 20:23:29 +0300

    add init

--- designs/apps.py
+++ designs/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class DesignsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'designs'
+
+
+
+
+
+    def ready(self):
+        import designs.signals  # Регистрируем сигналы
