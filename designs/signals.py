fa9d6e56c0a157ccdc40c258ae8359cf912a1698
Sapov <sapov@mail.ru>
2025-06-05 21:03:04 +0300

    create new signals

--- designs/signals.py
+++ designs/signals.py
@@ -3,31 +3,38 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.urls import reverse
-from .models import Comments
+from .models import Comments, OrderDesign
+from django.conf import settings
 
 
 @receiver(post_save, sender=Comments)
 def send_comment_notification(sender, instance, created, **kwargs):
-    if created:  # Только для новых комментариев
-        design = instance.design
-        project = design.project
+    if created:  # Отправляем только для новых комментариев
+        order_design = instance.design
+        author = instance.author
+        recipient = None
 
-        # Кому отправляем (клиенту или дизайнеру)
-        recipient = project.client if instance.author == project.designer else project.designer
-        subject = f'Новый комментарий к проекту "{project.title}"'
-        message = render_to_string('emails/new_comment.html', {
-            'recipient': recipient,
-            'author': instance.author,
-            'project': project,
-            'comment': instance,
-            'design_url': f'http://order.san-cd.ru/{reverse("project_detail", args=[project.id])}'
-        })
+        # Определяем получателя (если автор не владелец бриффа - отправляем владельцу)
+        if author != order_design.user and order_design.user:
+            recipient = order_design.user
 
-        send_mail(
-            subject,
-            message,
-            None,  # Используется DEFAULT_FROM_EMAIL
-            [recipient.email],
-            html_message=message,
-            fail_silently=False
-        )
+        # Если получатель определен и у него есть email
+        if recipient and recipient.email:
+            subject = f'Новый комментарий к брифу "{order_design.title}"'
+            message = render_to_string('emails/new_comment.html', {
+                'recipient': recipient,
+                'author': author,
+                'order_design': order_design,
+                'comment': instance,
+                'comment_url': settings.SITE_URL + reverse('designs:design_detail', args=[order_design.id]),
+                'unsubscribe_url': settings.SITE_URL + reverse('profile_settings')
+            })
+
+            send_mail(
+                subject,
+                message,
+                settings.DEFAULT_FROM_EMAIL,
+                [recipient.email],
+                html_message=message,
+                fail_silently=True
+            )
