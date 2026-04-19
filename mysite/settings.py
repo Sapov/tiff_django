35f0ad022eb7c2bb3a7bc63cbb82b5e9bcf2520e
Sapov <sapov@mail.ru>
2026-04-19 21:42:50 +0300

    edit settings.py / TRUSRED_ORIGINS

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOST")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOST").split(',')
 
 # Application definition
 
@@ -201,7 +201,7 @@ REST_FRAMEWORK = {
 }
 PHONENUMBER_DEFAULT_REGION = 'RU'
 
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
 if scrf_subdomain := os.getenv('SCRF_SUBDOMAIN'):
     CSRF_TRUSTED_ORIGINS += [f'http://{scrf_subdomain}', f'https://{scrf_subdomain}']
 
