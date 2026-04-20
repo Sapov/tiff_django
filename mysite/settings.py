501dd354c8da826072736076de633925091d8369
Sapov <sapov@mail.ru>
2026-04-20 12:14:30 +0300

    HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOST").split(',')
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOST").split(',')
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
