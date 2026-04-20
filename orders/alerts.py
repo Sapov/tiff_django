8ef5c6240bbc9e9d4ff6cc0a30e7b306f8200565
Sapov <sapov@mail.ru>
2026-04-20 11:42:33 +0300

    edit confirm_link

--- orders/alerts.py
+++ orders/alerts.py
@@ -41,6 +41,7 @@ class Alerts:
         msg.attach_alternative(html_message, "text/html")
         msg.send()
 
+
     def __generate_link_to_completed(self):
         '''Генерирую ссылку с уникальным ключом для перевода заказа в состояние в готов'''
         self.confirm_link_to_completed = (f'http://{self.domain}/confirm_order_to_competed/{self.order_id}/'
